8c949a4aa3c4f7b66ac4941b315e64e2faa5380b
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-15 23:25:49 +0000

    allow piping open strings without operators

--- src/pipe.ts
+++ src/pipe.ts
@@ -16,11 +16,15 @@ export function pipe<T, A, B, C, D, E, F, G, H, I>(i: Iterable<T>, p0: Piper<T, 
 export function pipe<T, A, B, C, D, E, F, G, H, I, J>(i: Iterable<T>, p0: Piper<T, A>, p1: Piper<A, B>, p2: Piper<B, C>, p3: Piper<C, D>, p4: Piper<D, E>, p5: Piper<E, F>, p6: Piper<F, G>, p7: Piper<G, H>, p8: Piper<H, I>, p9: Piper<I, J>): IterableExt<J>;
 
 export function pipe<T>(i: Iterable<T>, ...p: Piper<any, any>[]): IterableExt<any> {
-    const res = p.reduce((c, a) => a(c), i);
-    Object.defineProperty(res, 'first', ({
+    let r: any = p.reduce((c, a) => a(c), i);
+    if (typeof r === 'string') {
+        // operator-less iteration over an open string;
+        r = new String(r); // turn into object, so it can be extended
+    }
+    Object.defineProperty(r, 'first', ({
         get() {
             return this[Symbol.iterator]().next().value;
         }
     }));
-    return res;
+    return r;
 }
--- test/pipe.spec.ts
+++ test/pipe.spec.ts
@@ -2,10 +2,17 @@ import {expect} from './header';
 import {pipe} from '../src';
 
 describe('pipe', () => {
-    it('must work without operators', () => {
-        const input = [10, 20, 30];
-        const output = pipe(input);
-        expect([...output]).to.eql(input);
-        expect(output.first).to.eql(10);
+    describe('without operators', () => {
+        it('must work for iterable objects', () => {
+            const input = [10, 20, 30];
+            const output = pipe(input);
+            expect([...output]).to.eql(input);
+            expect(output.first).to.eql(10);
+        });
+        it('must work for strings', () => {
+            const output = pipe('one');
+            expect([...output]).to.eql(['o', 'n', 'e']);
+            expect(output.first).to.eql('o');
+        });
     });
 });
