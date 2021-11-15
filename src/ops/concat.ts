b0b31670a516a51f1450b82ba3fa033f06ec9d90
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-15 14:52:34 +0000

    started working on concat operator

---
+++ src/ops/concat.ts
@@ -0,0 +1,19 @@
+import {Piper} from '../types';
+
+/**
+ * Logically merges current iterable with same-type list of values or iterables.
+ * Merged values are iterated over after the current iterable is depleted.
+ *
+ * See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+ */
+export function concat<T>(...values: (T | Iterable<T>)[]): Piper<T, T> {
+    return (iterable: Iterable<T>) => ({
+        [Symbol.iterator](): Iterator<T> {
+            return {
+                next(): IteratorResult<T> {
+
+                }
+            };
+        }
+    });
+}
