75e8c9a800d038b03b14257675eddc5bdf39f482
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-16 12:58:42 +0000

    distinct finished

--- src/ops/distinct.ts
+++ src/ops/distinct.ts
@@ -1,26 +1,28 @@
 import {Piper} from '../types';
 
 /**
- * See here: https://github.com/ReactiveX/rxjs/blob/6fa819beb91ba99dadd6262d6c13f7ddfd9470c5/src/internal/operators/distinct.ts
+ * Emits unique values, with optional key selector.
  */
-export function distinct<T, K>(cb?: (value: T) => K): Piper<T, T> {
+export function distinct<T, K>(keySelector?: (value: T, index: number) => K): Piper<T, T> {
     return (iterable: Iterable<T>) => ({
         [Symbol.iterator](): Iterator<T> {
             const i = iterable[Symbol.iterator]();
             const keySet = new Set();
+            let index = 0;
             return {
                 next(): IteratorResult<T> {
                     let a;
                     do {
                         a = i.next();
                         if (!a.done) {
-                            const key = cb ? cb(a.value) : a.value;
+                            const key = keySelector ? keySelector(a.value, index++) : a.value;
                             if (!keySet.has(key)) {
                                 keySet.add(key);
                                 return a;
                             }
                         }
                     } while (!a.done);
+                    keySet.clear();
                     return {value: undefined, done: true};
                 }
             };
