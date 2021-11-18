706586835532faef3b9d61f8af3a7b896ce2800f
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-18 02:25:37 +0000

    improve takeLast

--- src/ops/take-last.ts
+++ src/ops/take-last.ts
@@ -8,21 +8,24 @@ export function takeLast<T>(count: number): Piper<T, T> {
         [Symbol.iterator](): Iterator<T> {
             const i = iterable[Symbol.iterator]();
             const buffer: IteratorResult<T>[] = [];
-            let ready = false, index = 0;
+            let ready = false, done = false, index = 0;
             return {
                 next(): IteratorResult<T> {
-                    if (!ready) {
-                        let a;
-                        while (!(a = i.next()).done) {
-                            buffer.push(a);
-                            if (count < buffer.length) {
-                                buffer.shift();
+                    if (!done && count > 0) {
+                        if (!ready) {
+                            let a;
+                            while (!(a = i.next()).done) {
+                                buffer.push(a);
+                                if (count < buffer.length) {
+                                    buffer.shift();
+                                }
                             }
+                            ready = true;
                         }
-                        ready = true;
-                    }
-                    if (index < buffer.length) {
-                        return buffer[index++];
+                        if (index < buffer.length) {
+                            return buffer[index++];
+                        }
+                        done = true;
                     }
                     return {value: undefined, done: true};
                 }
