0f88a32961d3643548a8e3af852e4e8b9ab55958
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-18 03:36:23 +0000

    declarations all sorted

--- src/ops/catch-error.ts
+++ src/ops/catch-error.ts
@@ -1,19 +1,28 @@
-import {Piper} from '../types';
+import {Any, Piper} from '../types';
 
 /**
- * Catches iteration errors, to either re-throw or return a new value.
+ * Catches iteration errors, to either re-throw or return a new value|iterator|iterable.
  */
-export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => never): Piper<T, T>;
-export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => T): Piper<T, T>;
+export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => never): Piper<T, never>;
+export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => Any<T>): Piper<T, T>;
 
-export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => T | never): Piper<T, T> {
+export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => Any<T> | never): Piper<T, T> {
     return (iterable: Iterable<T>) => ({
         [Symbol.iterator](): Iterator<T> {
-            // const i = iterable[Symbol.iterator]();
-            // let index = 0;
+            const i = iterable[Symbol.iterator]();
+            let index = 0, last: T;
+            // TODO: this is all work in progress!
             return {
                 next(): IteratorResult<T> {
-                    // TODO: to be implemented
+                    let a;
+                    try {
+                        a = i.next();
+                        last = a.value;
+                    } catch (e) {
+                        const r = cb(e, index, last) as T;
+                        return {value: r};
+                    }
+                    index++;
                     return {value: undefined, done: true};
                 }
             };
