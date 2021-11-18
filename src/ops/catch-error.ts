e33e4157209b4ee9f6e62e6919dc4218f0687b66
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-18 04:58:56 +0000

    fixing catchError signature

--- src/ops/catch-error.ts
+++ src/ops/catch-error.ts
@@ -3,10 +3,10 @@ import {Piper} from '../types';
 /**
  * Catches iteration errors, to either re-throw or return a new value.
  */
-export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => never): Piper<T, never>;
+export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => never): Piper<T, T>;
 export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => T): Piper<T, T>;
 
-export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => T | never): Piper<T, T> {
+export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => T): Piper<T, T> {
     return (iterable: Iterable<T>) => ({
         [Symbol.iterator](): Iterator<T> {
             const i = iterable[Symbol.iterator]();
