0f2125f48acde3d1130963ed7860ca6ec5f945ef
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-19 10:26:14 +0000

    started rewriting errors

--- src/ops/catch-error.ts
+++ src/ops/catch-error.ts
@@ -1,5 +1,12 @@
 import {Piper} from '../types';
 
+export interface IErrorInfo<T> {
+    index: number;
+    lastValue?: T;
+
+    emit(value: T): void;
+}
+
 /**
  * Catches iteration errors, to either re-throw or return a new value of the same type.
  *
@@ -9,23 +16,38 @@ import {Piper} from '../types';
  *  - index: index of the value that we failed to retrieve
  *  - lastValue: last successful value, if any, or else undefined
  */
-export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => never): Piper<T, T>;
-export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => T): Piper<T, T>;
-
-export function catchError<T>(cb: (error: any, index: number, lastValue?: T) => T): Piper<T, T> {
+export function catchError<T>(cb: (error: any, info: IErrorInfo<T>) => never): Piper<T, T> {
     return (iterable: Iterable<T>) => ({
         [Symbol.iterator](): Iterator<T> {
             const i = iterable[Symbol.iterator]();
             let index = 0, last: IteratorResult<T>;
             return {
                 next(): IteratorResult<T> {
-                    try {
-                        last = i.next();
-                    } catch (e) {
-                        return {value: cb(e, index++, last?.value)};
-                    }
-                    index++;
-                    return last;
+                    do {
+                        try {
+                            last = i.next();
+                        } catch (e) {
+                            let value: T, emitted = false;
+                            const info: IErrorInfo<T> = {
+                                index: index++,
+                                lastValue: last?.value,
+                                emit(v) {
+                                    value = v;
+                                    emitted = true;
+                                }
+                            };
+                            cb(e, info);
+                            if (emitted) {
+                                return {value};
+                            }
+                            // TODO: what do we do here?
+                        }
+                        index++;
+                        if (!last?.done) {
+                            return last;
+                        }
+                    } while (!last.done);
+                    return {value: undefined, done: true};
                 }
             };
         }
