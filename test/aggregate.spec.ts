65ff54a489f79e9f095b108b77a0f6e3b3b7ddd0
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-24 15:00:27 +0000

    aggregate finished

--- test/aggregate.spec.ts
+++ test/aggregate.spec.ts
@@ -1,8 +1,22 @@
 import {expect} from './header';
-// import {pipe, aggregate} from '../src';
+import {pipe, aggregate} from '../src';
 
 describe('aggregate', () => {
-    it('must do something', () => {
-        expect(true).to.be.true;
+    it('must process data correctly', () => {
+        const output = pipe([1, 2, 3], aggregate(arr => {
+            return arr.reduce((a, c) => a + c);
+        }));
+        expect([...output]).to.eql([6]);
+    });
+    it('must handle empty iterables', () => {
+        const output = pipe([], aggregate(arr => {
+            return arr;
+        }));
+        expect([...output]).to.eql([[]]);
+    });
+    it('must allow return of nothing', () => {
+        const output = pipe([], aggregate(arr => {
+        }));
+        expect([...output]).to.eql([undefined]);
     });
 });
