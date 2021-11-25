0c4a11a1ede7c8c3ba5fbd28ab5ec81f351d2b22
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-25 13:58:23 +0000

    adding tests

--- test/page.spec.ts
+++ test/page.spec.ts
@@ -3,12 +3,36 @@ import {pipe, page} from '../src';
 
 describe('page', () => {
     it('must handle divisible page sizes', () => {
-
+        const input = [1, 2, 3, 4, 5, 6];
+        const i = pipe(input, page(3));
+        expect([...i]).to.eql([[1, 2, 3], [4, 5, 6]]);
     });
     it('must handle non-divisible page sizes', () => {
+        const input1 = [1, 2, 3, 4, 5];
+        const i1 = pipe(input1, page(3));
+        expect([...i1], 'more than 1 page').to.eql([[1, 2, 3], [4, 5]]);
 
+        const input2 = [1, 2];
+        const i2 = pipe(input2, page(3));
+        expect([...i2], 'less then 1 page').to.eql([[1, 2]]);
     });
     it('must handle empty iterables', () => {
-
+        const i = pipe([], page(3));
+        expect([...i]).to.eql([]);
+    });
+    it('must handle page size of 1', () => {
+        const i = pipe([1, 2, 3], page(1));
+        expect([...i]).to.eql([[1], [2], [3]]);
+    });
+    it('must throw on page size < 1 or invalid', () => {
+        expect(() => {
+            pipe([], page(0));
+        }).to.throw('ops!');
+        expect(() => {
+            pipe([], page(-1));
+        }).to.throw('ops!');
+        expect(() => {
+            pipe([], page('bla' as any));
+        }).to.throw('ops!');
     });
 });
