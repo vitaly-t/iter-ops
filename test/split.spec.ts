de8fd3cba7a1dbc11a1998e122e47f4564102f09
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-27 19:16:34 +0000

    coverage complete

--- test/split.spec.ts
+++ test/split.spec.ts
@@ -1,5 +1,5 @@
 import {expect} from './header';
-import {pipe, split} from '../src';
+import {pipe, split, SplitValueCarry} from '../src';
 
 describe('split', () => {
     describe('without options', () => {
@@ -54,54 +54,72 @@ describe('split', () => {
             });
         });
         describe('toggle', () => {
-            it('must handle any regular scenario', () => {
-                const i1 = pipe([0, 1, 2, 0, 0, 3, 4], split(a => !a, {toggle: true}));
-                expect([...i1]).to.eql([[1, 2], [3, 4]]);
+            describe('without carrying', () => {
+                it('must handle any regular scenario', () => {
+                    const i1 = pipe([0, 1, 2, 0, 0, 3, 4], split(a => !a, {
+                        toggle: true,
+                        carryStart: 'bla' as any, // for coverage
+                        carryEnd: 'bla' as any // for coverage
+                    }));
+                    expect([...i1]).to.eql([[1, 2], [3, 4]]);
 
-                const i2 = pipe([1, 2, 0, 0, 3, 4, 0], split(a => !a, {toggle: true}));
-                expect([...i2]).to.eql([[], []]);
+                    const i2 = pipe([1, 2, 0, 0, 3, 4, 0], split(a => !a, {
+                        toggle: true,
+                        carryStart: SplitValueCarry.none // just for coverage
+                    }));
+                    expect([...i2]).to.eql([[], []]);
 
-                const i3 = pipe([1, 2, 0, 3, 4, 0], split(a => !a, {toggle: true}));
-                expect([...i3]).to.eql([[3, 4]]);
+                    const i3 = pipe([1, 2, 0, 3, 4, 0], split(a => !a, {toggle: true}));
+                    expect([...i3]).to.eql([[3, 4]]);
+                });
+                it('must handle no toggles', () => {
+                    const i = pipe([1, 2, 3, 4, 5], split(a => false, {toggle: true}));
+                    expect([...i]).to.eql([]);
+                });
+                it('must handle all toggles', () => {
+                    // ending with open toggle:
+                    const i1 = pipe([1, 2, 3, 4, 5], split(a => true, {toggle: true}));
+                    expect([...i1]).to.eql([[], [], []]);
+
+                    // ending with closed toggle:
+                    const i2 = pipe([1, 2, 3, 4, 5, 6], split(a => true, {toggle: true}));
+                    expect([...i2]).to.eql([[], [], []]);
+                });
+                it('must process trim correctly', () => {
+                    const i1 = pipe([1], split(a => true, {toggle: true, trim: true}));
+                    expect([...i1]).to.eql([]);
+
+                    const i2 = pipe([1, 2, 3, 4, 5], split(a => true, {toggle: true, trim: true}));
+                    expect([...i2]).to.eql([]);
+                });
             });
-            it('must handle no toggles', () => {
-                const i = pipe([1, 2, 3, 4, 5], split(a => false, {toggle: true}));
-                expect([...i]).to.eql([]);
-            });
-            it('must handle all toggles', () => {
-                // ending with open toggle:
-                const i1 = pipe([1, 2, 3, 4, 5], split(a => true, {toggle: true}));
-                expect([...i1]).to.eql([[], [], []]);
 
-                // ending with closed toggle:
-                const i2 = pipe([1, 2, 3, 4, 5, 6], split(a => true, {toggle: true}));
-                expect([...i2]).to.eql([[], [], []]);
-            });
-            it('must process trim correctly', () => {
-                const i1 = pipe([1], split(a => true, {toggle: true, trim: true}));
-                expect([...i1]).to.eql([]);
+            describe('with carrying', () => {
+                it('must work with carrying back', () => {
+                    const i1 = pipe([1, 2, 3, 4, 5], split(a => true, {toggle: true, carryStart: -1}));
+                    expect([...i1]).to.eql([[1], [3], [5]]);
 
-                const i2 = pipe([1, 2, 3, 4, 5], split(a => true, {toggle: true, trim: true}));
-                expect([...i2]).to.eql([]);
-            });
-            it('must work with carrying back', () => {
-                const i1 = pipe([1, 2, 3, 4, 5], split(a => true, {toggle: true, carryStart: -1}));
-                expect([...i1]).to.eql([[1], [3], [5]]);
+                    const i2 = pipe([1, 2, 3, 4, 5], split(a => true, {toggle: true, carryStart: -1, carryEnd: -1}));
+                    expect([...i2]).to.eql([[1, 2], [3, 4], [5]]);
+                });
+                it('must work with carrying forward', () => {
+                    const i1 = pipe([1, 2, 3, 4, 5], split(a => !!a, {toggle: true, carryStart: 1}));
+                    expect([...i1]).to.eql([[1], [3], [5]]);
 
-                const i2 = pipe([1, 2, 3, 4, 5], split(a => true, {toggle: true, carryStart: -1, carryEnd: -1}));
-                expect([...i2]).to.eql([[1, 2], [3, 4], [5]]);
-            });
-            it('must work with carrying forward', () => {
-                const i1 = pipe([1, 2, 3, 4, 5], split(a => !!a, {toggle: true, carryStart: 1}));
-                expect([...i1]).to.eql([[1], [3], [5]]);
+                    const i2 = pipe([0, 1, 2, 0, 3, 4, 0, 5], split(a => a === 0, {
+                        toggle: true,
+                        carryStart: 1,
+                        carryEnd: 1
+                    }));
+                    expect([...i2]).to.eql([[0, 1, 2], [0, 0, 5]]);
+                });
+                it('must handle triggers without values', () => {
+                    const i1 = pipe([0, 0, 0, 0], split(a => a === 0, {toggle: true, carryStart: 1, carryEnd: 1}));
+                    expect([...i1]).to.eql([[0], [0, 0]]);
 
-                /*
-                const i2 = pipe([0, 1, 2, 0, 3, 4, 0, 5], split(a => a === 0, {
-                    toggle: true,
-                    carryStart: 1,
-                    carryEnd: 1
-                }));
-                expect([...i2]).to.eql([[], [1, 2], [3, 4], [5]]);*/
+                    const i2 = pipe([0, 0, 0, 0], split(a => a === 0, {toggle: true, carryStart: -1, carryEnd: -1}));
+                    expect([...i2]).to.eql([[0, 0], [0, 0]]);
+                });
             });
         });
     });
