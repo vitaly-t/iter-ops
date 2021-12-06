d207f7b75ff26b4c583bbafabf509213f66f90f0
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-06 12:39:17 +0000

    adding more operators

--- src/ops/count.ts
+++ src/ops/count.ts
@@ -1,12 +1,16 @@
 import {Operation} from '../types';
+import {createOperation} from '../utils';
 
 /**
  * Goes through the entire iterable, counting the values,
  * and produces a one-value iterable with the count.
  */
 export function count<T>(): Operation<T, number> {
-    return null as any;/*
-    return (iterable: Iterable<T>) => ({
+    return createOperation(countSync, countAsync);
+}
+
+function countSync<T>(iterable: Iterable<T>): Iterable<number> {
+    return {
         [Symbol.iterator](): Iterator<number> {
             let done = false;
             return {
@@ -25,5 +29,28 @@ export function count<T>(): Operation<T, number> {
                 }
             };
         }
-    });*/
+    };
+}
+
+function countAsync<T>(iterable: AsyncIterable<T>): AsyncIterable<number> {
+    return {
+        [Symbol.asyncIterator](): AsyncIterator<number> {
+            let done = false;
+            return {
+                async next(): Promise<IteratorResult<number>> {
+                    let value;
+                    if (done) {
+                        return {value, done};
+                    }
+                    const i = iterable[Symbol.asyncIterator]();
+                    value = 0;
+                    while (!(await i.next()).done) {
+                        value++;
+                    }
+                    done = true;
+                    return {value};
+                }
+            };
+        }
+    };
 }
--- src/ops/distinct.ts
+++ src/ops/distinct.ts
@@ -1,11 +1,15 @@
 import {Operation} from '../types';
+import {createOperation} from '../utils';
 
 /**
  * Emits unique values, with optional key selector.
  */
 export function distinct<T, K>(keySelector?: (value: T, index: number) => K): Operation<T, T> {
-    return null as any;/*
-    return (iterable: Iterable<T>) => ({
+    return createOperation(distinctSync, distinctAsync, arguments);
+}
+
+function distinctSync<T, K>(iterable: Iterable<T>, keySelector?: (value: T, index: number) => K): Iterable<T> {
+    return {
         [Symbol.iterator](): Iterator<T> {
             const i = iterable[Symbol.iterator]();
             const ks = typeof keySelector === 'function' && keySelector;
@@ -29,5 +33,33 @@ export function distinct<T, K>(keySelector?: (value: T, index: number) => K): Op
                 }
             };
         }
-    });*/
+    };
+}
+
+function distinctAsync<T, K>(iterable: AsyncIterable<T>, keySelector?: (value: T, index: number) => K): AsyncIterable<T> {
+    return {
+        [Symbol.asyncIterator](): AsyncIterator<T> {
+            const i = iterable[Symbol.asyncIterator]();
+            const ks = typeof keySelector === 'function' && keySelector;
+            const keySet = new Set();
+            let index = 0;
+            return {
+                async next(): Promise<IteratorResult<T>> {
+                    let a;
+                    do {
+                        a = await i.next();
+                        if (!a.done) {
+                            const key = ks ? ks(a.value, index++) : a.value;
+                            if (!keySet.has(key)) {
+                                keySet.add(key);
+                                return a;
+                            }
+                        }
+                    } while (!a.done);
+                    keySet.clear(); // for better memory management
+                    return a;
+                }
+            };
+        }
+    };
 }
