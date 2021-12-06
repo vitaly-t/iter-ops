524edba356b4c6c97ce23e5b2f62e6ee952236b7
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-06 12:52:43 +0000

    adding isEmpty operator

--- src/ops/is-empty.ts
+++ src/ops/is-empty.ts
@@ -1,12 +1,16 @@
 import {Operation} from '../types';
+import {createOperation} from '../utils';
 
 /**
  * Checks if the iterable can produce any value,
  * and returns a one-value iterable with the flag.
  */
 export function isEmpty<T>(): Operation<T, boolean> {
-    return null as any;/*
-    return (iterable: Iterable<T>) => ({
+    return createOperation(isEmptySync, isEmptyAsync);
+}
+
+function isEmptySync<T>(iterable: Iterable<T>): Iterable<boolean> {
+    return {
         [Symbol.iterator](): Iterator<boolean> {
             let done = false;
             return {
@@ -20,5 +24,23 @@ export function isEmpty<T>(): Operation<T, boolean> {
                 }
             };
         }
-    });*/
+    };
+}
+
+function isEmptyAsync<T>(iterable: AsyncIterable<T>): AsyncIterable<boolean> {
+    return {
+        [Symbol.asyncIterator](): AsyncIterator<boolean> {
+            let done = false;
+            return {
+                async next(): Promise<IteratorResult<boolean>> {
+                    if (done) {
+                        return {value: undefined, done};
+                    }
+                    const a = await iterable[Symbol.asyncIterator]().next();
+                    done = true;
+                    return {value: !!a.done};
+                }
+            };
+        }
+    };
 }
