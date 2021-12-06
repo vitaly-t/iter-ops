1672bbc7fbc1dc3c591b402d9bf7a9aca7e609c9
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-06 12:49:01 +0000

    adding empty operator

--- src/ops/empty.ts
+++ src/ops/empty.ts
@@ -1,11 +1,25 @@
 import {Operation} from '../types';
+import {createOperation} from '../utils';
 
 /**
  * Produces an empty iterable.
  */
 export function empty<T>(): Operation<T, T> {
-    return null as any;/*
-    return () => ({
+    return createOperation(emptySync, emptyAsync);
+}
+
+function emptySync<T>(): Iterable<T> {
+    return {
         [Symbol.iterator]: () => ({next: () => ({value: undefined, done: true})})
-    });*/
+    };
+}
+
+function emptyAsync<T>(): AsyncIterable<T> {
+    return {
+        [Symbol.asyncIterator]: () => ({
+            async next() {
+                return {value: undefined, done: true};
+            }
+        })
+    };
 }
