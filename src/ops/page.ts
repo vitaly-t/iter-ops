8476aea3714d4ce0dcf720da7637b066714aa423
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-06 13:11:46 +0000

    adding page + skip operators

--- src/ops/page.ts
+++ src/ops/page.ts
@@ -1,4 +1,5 @@
 import {Operation} from '../types';
+import {createOperation} from '../utils';
 
 /**
  * Splits values into pages of fixed size (last page can be smaller).
@@ -6,8 +7,11 @@ import {Operation} from '../types';
  * Throws an error when page size is less than 1 or not a number.
  */
 export function page<T>(size: number): Operation<T, T[]> {
-    return null as any;/*
-    return (iterable: Iterable<T>) => ({
+    return createOperation(pageSync, pageAsync, arguments);
+}
+
+function pageSync<T>(iterable: Iterable<T>, size: number): Iterable<T[]> {
+    return {
         [Symbol.iterator](): Iterator<T[]> {
             const i = iterable[Symbol.iterator]();
             let start = true;
@@ -31,5 +35,33 @@ export function page<T>(size: number): Operation<T, T[]> {
                 }
             };
         }
-    });*/
+    };
+}
+
+function pageAsync<T>(iterable: AsyncIterable<T>, size: number): AsyncIterable<T[]> {
+    return {
+        [Symbol.asyncIterator](): AsyncIterator<T[]> {
+            const i = iterable[Symbol.asyncIterator]();
+            let start = true;
+            return {
+                async next(): Promise<IteratorResult<T[]>> {
+                    if (start) {
+                        if (typeof size !== 'number' || size < 1) {
+                            throw new TypeError(`Page size >= 1 is required: ${JSON.stringify(size)}`);
+                        }
+                        start = false;
+                    }
+                    const value = [];
+                    let a, c = 0;
+                    while (c++ < size && !(a = await i.next()).done) {
+                        value.push(a.value);
+                    }
+                    if (value.length) {
+                        return {value};
+                    }
+                    return {value: undefined, done: true};
+                }
+            };
+        }
+    };
 }
