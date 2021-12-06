94fd496e5fc09ceb69d316859f64ed8393a29ffd
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-06 12:30:24 +0000

    adding take + takeLast

--- src/ops/take-last.ts
+++ src/ops/take-last.ts
@@ -1,11 +1,15 @@
 import {Operation} from '../types';
+import {createOperation} from '../utils';
 
 /**
  * Emits up to "count" number of the last values.
  */
 export function takeLast<T>(count: number): Operation<T, T> {
-    return null as any;/*
-    return (iterable: Iterable<T>) => ({
+    return createOperation(takeLastSync, takeLastAsync, arguments);
+}
+
+function takeLastSync<T>(iterable: Iterable<T>, count: number): Iterable<T> {
+    return {
         [Symbol.iterator](): Iterator<T> {
             const i = iterable[Symbol.iterator]();
             const buffer: IteratorResult<T>[] = [];
@@ -32,5 +36,36 @@ export function takeLast<T>(count: number): Operation<T, T> {
                 }
             };
         }
-    });*/
+    };
+}
+
+function takeLastAsync<T>(iterable: AsyncIterable<T>, count: number): AsyncIterable<T> {
+    return {
+        [Symbol.asyncIterator](): AsyncIterator<T> {
+            const i = iterable[Symbol.asyncIterator]();
+            const buffer: IteratorResult<T>[] = [];
+            let ready = false, done = false, index = 0;
+            return {
+                async next(): Promise<IteratorResult<T>> {
+                    if (!done && count > 0) {
+                        if (!ready) {
+                            let a;
+                            while (!(a = await i.next()).done) {
+                                buffer.push(a);
+                                if (count < buffer.length) {
+                                    buffer.shift();
+                                }
+                            }
+                            ready = true;
+                        }
+                        if (index < buffer.length) {
+                            return buffer[index++];
+                        }
+                        done = true;
+                    }
+                    return {value: undefined, done: true};
+                }
+            };
+        }
+    };
 }
--- src/ops/take.ts
+++ src/ops/take.ts
@@ -1,11 +1,15 @@
 import {Operation} from '../types';
+import {createOperation} from '../utils';
 
 /**
  * Emits up to "count" number of values.
  */
 export function take<T>(count: number): Operation<T, T> {
-    return null as any;/*
-    return (iterable: Iterable<T>) => ({
+    return createOperation(takeSync, takeAsync, arguments);
+}
+
+function takeSync<T>(iterable: Iterable<T>, count: number): Iterable<T> {
+    return {
         [Symbol.iterator](): Iterator<T> {
             const i = iterable[Symbol.iterator]();
             let index = 0, done = false;
@@ -23,5 +27,27 @@ export function take<T>(count: number): Operation<T, T> {
                 }
             };
         }
-    });*/
+    };
+}
+
+function takeAsync<T>(iterable: AsyncIterable<T>, count: number): AsyncIterable<T> {
+    return {
+        [Symbol.asyncIterator](): AsyncIterator<T> {
+            const i = iterable[Symbol.asyncIterator]();
+            let index = 0, done = false;
+            return {
+                async next(): Promise<IteratorResult<T>> {
+                    if (!done) {
+                        const a = await i.next();
+                        if (a.done || index++ >= count) {
+                            done = true;
+                        } else {
+                            return a;
+                        }
+                    }
+                    return {value: undefined, done};
+                }
+            };
+        }
+    };
 }
