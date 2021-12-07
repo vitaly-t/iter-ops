5ceddba091612392f88b8704d74ce26ca493743d
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-07 22:24:27 +0000

    adding async toArray tests

--- test/to-array.spec.ts
+++ test/to-array.spec.ts
@@ -1,7 +1,7 @@
-import {expect} from './header';
+import {_async, expect} from './header';
 import {pipe, toArray} from '../src';
 
-describe('toArray', () => {
+describe('sync toArray', () => {
     it('must recreate input array', () => {
         const input = [1, 2, 3];
         const output = pipe(input, toArray());
@@ -12,6 +12,21 @@ describe('toArray', () => {
         const output = pipe(input, toArray());
         const i = output[Symbol.iterator]();
         const result = i.next() && i.next();
+        expect(result).to.eql({value: undefined, done: true});
+    });
+});
+
+describe('async toArray', () => {
+    it('must recreate input array', async () => {
+        const input = [1, 2, 3];
+        const output = pipe(_async(input), toArray());
+        expect(await output.first).to.eql(input);
+    });
+    it('must not generate more than one value', async () => {
+        const input = _async([1, 2]);
+        const output = pipe(input, toArray());
+        const i = output[Symbol.asyncIterator]();
+        const result = await i.next() && await i.next();
         expect(result).to.eql({value: undefined, done: true});
     });
 });
