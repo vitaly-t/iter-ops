2d408ec74f994158c3c7cc0b1451de94abaadd82
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-07 22:15:24 +0000

    adding async filter tests

--- test/filter.spec.ts
+++ test/filter.spec.ts
@@ -1,7 +1,7 @@
-import {expect} from './header';
+import {createAsync, expect, getAsyncValues} from './header';
 import {pipe, filter} from '../src';
 
-describe('filter', () => {
+describe('sync filter', () => {
     it('must emit on condition', () => {
         const input = [1, 2, 3, 4, 5, 0];
         const output = pipe(input, filter(a => a < 3));
@@ -17,6 +17,26 @@ describe('filter', () => {
             return false;
         }));
         [...output];
+        expect(arr).to.eql([1, 2, 3, 4, 5, 6]);
+    });
+});
+
+describe('async filter', () => {
+    it('must emit on condition', async () => {
+        const input = createAsync([1, 2, 3, 4, 5, 0]);
+        const output = pipe(input, filter(a => a < 3));
+        expect(await getAsyncValues(output)).to.eql([1, 2, 0]);
+    });
+    it('must reuse the state object', async () => {
+        const input = createAsync('hello!');
+        const arr: number[] = [];
+        const output = pipe(input, filter((value, index, state) => {
+            state.count = state.count ?? 0;
+            state.count++;
+            arr.push(state.count);
+            return false;
+        }));
+        await getAsyncValues(output);
         expect(arr).to.eql([1, 2, 3, 4, 5, 6]);
     });
 });
