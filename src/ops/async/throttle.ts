5ebc5f7e341d920081a742a033852d67bb721c4d
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-08 22:56:38 +0000

    throttle finished

--- src/ops/async/throttle.ts
+++ src/ops/async/throttle.ts
@@ -2,22 +2,22 @@ import {IterationState, Operation} from '../../types';
 import {createOperation, throwOnSync} from '../../utils';
 
 /**
- * Emits each value after the callback result resolves or emits a value.
- * The resolved/emitted value itself is ignored.
+ * Emits each value after the callback result resolves.
+ * The resolved value itself is ignored.
  */
-export function throttle<T>(cb: (value: T, index: number, state: IterationState) => AsyncIterable<any> | Promise<any>): Operation<T, T> {
+export function throttle<T>(cb: (value: T, index: number, state: IterationState) => Promise<any>): Operation<T, T> {
     return createOperation(throwOnSync('throttle'), throttleAsync, arguments);
 }
 
-function throttleAsync<T>(iterable: AsyncIterable<T>, cb: (value: T, index: number, state: IterationState) => AsyncIterable<T> | Promise<T>): AsyncIterable<T> {
+function throttleAsync<T>(iterable: AsyncIterable<T>, cb: (value: T, index: number, state: IterationState) => Promise<T>): AsyncIterable<T> {
     return {
         [Symbol.asyncIterator](): AsyncIterator<T> {
             const i = iterable[Symbol.asyncIterator]();
-            // const state: IterationState = {};
-            // let index = 0;
+            const state: IterationState = {};
+            let index = 0;
             return {
                 next(): Promise<IteratorResult<T>> {
-                    return i.next(); // TODO: to be implemented
+                    return i.next().then(a => a.done ? a : cb(a.value, index++, state).then(() => a));
                 }
             };
         }
--- test/async/throttle.spec.ts
+++ test/async/throttle.spec.ts
@@ -3,7 +3,17 @@ import {pipe, throttle} from '../../src';
 
 describe('throttle', () => {
     it('must emit after resolved', async () => {
-        const output = pipe(_async([1]), throttle(() => Promise.resolve(123)));
-        expect(await _asyncValues(output)).to.eql([1]);
+        const params: any[] = [];
+        const output = pipe(_async([1, 2, 3]), throttle(async (value, index, state) => {
+            state.sum = (state.sum || 0) + value;
+            params.push({value, index, state});
+            return 123;
+        }));
+        expect(await _asyncValues(output)).to.eql([1, 2, 3]);
+        expect(params).to.eql([
+            {value: 1, index: 0, state: {sum: 6}},
+            {value: 2, index: 1, state: {sum: 6}},
+            {value: 3, index: 2, state: {sum: 6}}
+        ]);
     });
 });
