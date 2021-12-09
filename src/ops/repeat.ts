686af081df672c3e2fe67f07948a281d8c936f00
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-09 14:58:51 +0000

    sync repeat finished

---
+++ src/ops/repeat.ts
@@ -0,0 +1,102 @@
+import {IterationState, Operation} from '../types';
+import {createOperation} from '../utils';
+
+/**
+ * Repeats every value specified number of times.
+ */
+export function repeat<T>(count: number): Operation<T, T>;
+
+/**
+ * Repeats every value, while the callback is returning true.
+ * `index` - original value index;
+ * `count` - repeats count thus far (starts with 0).
+ */
+export function repeat<T>(cb: (value: T, index: number, count: number, state: IterationState) => boolean): Operation<T, T>;
+
+/**
+ * Repeats every value, for as long as the callback result resolves with true.
+ * `index` - original value index;
+ * `count` - repeats count thus far (starts with 0).
+ *
+ * This works only inside asynchronous iterables.
+ */
+export function repeat<T>(cb: (value: T, index: number, count: number, state: IterationState) => Promise<boolean>): Operation<T, T>;
+
+/**
+ * Starts emitting values after "count" number of values.
+ */
+export function repeat<T>(count: number | ((value: T, index: number, count: number, state: IterationState) => boolean | Promise<boolean>)): Operation<T, T> {
+    return createOperation(repeatSync, repeatAsync, arguments);
+}
+
+function repeatSync<T>(iterable: Iterable<T>, count: number | ((value: T, index: number, count: number, state: IterationState) => boolean)): Iterable<T> {
+    return {
+        [Symbol.iterator](): Iterator<T> {
+            const i = iterable[Symbol.iterator]();
+            const state: IterationState = {};
+            const cb = typeof count === 'function' && count;
+            const getCopyCount = () => !cb && count > 0 ? count as number : 0;
+            let copyCount = getCopyCount();
+            let index = -1, copied = 0, start = true, a: IteratorResult<T>;
+            return {
+                next(): IteratorResult<T> {
+                    if (start) {
+                        a = i.next();
+                        start = false;
+                        index++;
+                        copied = 0;
+                        copyCount = getCopyCount();
+                    }
+                    if (a.done) {
+                        return a;
+                    }
+                    if (cb) {
+                        start = !cb(a.value, index, copied++, state);
+                        return a;
+                    }
+                    if (copyCount) {
+                        copyCount--;
+                    } else {
+                        start = true;
+                    }
+                    return a;
+                }
+            };
+        }
+    };
+}
+
+function repeatAsync<T>(iterable: AsyncIterable<T>, count: number | ((value: T, index: number, count: number, state: IterationState) => boolean | Promise<boolean>)): AsyncIterable<T> {
+    return {
+        [Symbol.asyncIterator](): AsyncIterator<T> {
+            //const i = iterable[Symbol.asyncIterator]();
+            //const cb = typeof count === 'function' && count;
+            //const getCopyCount = () => !cb && count > 0 ? count as number : 0;
+            //const copyCount = getCopyCount();
+            //let index = 0;
+            return {
+                next(): Promise<IteratorResult<T>> {
+                    return null as any;
+                    /*
+                    return i.next()
+                        .then(a => {
+                            index++;
+                            attempts = 0;
+                            leftTries = getLeftTries();
+                            return a;
+                        })
+                        .catch(e => {
+                            if (cb) {
+                                return cb(index, attempts++, state).then(r => r ? this.next() : Promise.reject(e));
+                            }
+                            if (leftTries) {
+                                leftTries--;
+                                return this.next();
+                            }
+                            return Promise.reject(e);
+                        });*/
+                }
+            };
+        }
+    };
+}
---
+++ test/repeat.spec.ts
@@ -0,0 +1,38 @@
+import {expect} from './header';
+import {pipe, repeat} from '../src';
+
+describe('sync repeat', () => {
+    it('must not copy for 0 or negative count', () => {
+        const i1 = pipe([1, 2], repeat(0));
+        expect([...i1]).to.eql([1, 2]);
+        const i2 = pipe([1, 2], repeat(-1));
+        expect([...i2]).to.eql([1, 2]);
+    });
+    it('must copy for positive numbers', () => {
+        const i1 = pipe([1, 2], repeat(1));
+        expect([...i1]).to.eql([1, 1, 2, 2]);
+        const i2 = pipe([1, 2], repeat(2));
+        expect([...i2]).to.eql([1, 1, 1, 2, 2, 2]);
+    });
+    it('must copy for callbacks', () => {
+        const i1 = pipe([1, 2], repeat((value, index, count) => count < 1));
+        expect([...i1]).to.eql([1, 1, 2, 2]);
+        const params: any[] = [];
+        const i2 = pipe([1, 2], repeat((value, index, count) => {
+            params.push({value, index, count});
+            return count < 2;
+        }));
+        expect([...i2]).to.eql([1, 1, 1, 2, 2, 2]);
+        expect(params).to.eql([
+            {value: 1, index: 0, count: 0},
+            {value: 1, index: 0, count: 1},
+            {value: 1, index: 0, count: 2},
+            {value: 2, index: 1, count: 0},
+            {value: 2, index: 1, count: 1},
+            {value: 2, index: 1, count: 2}
+        ]);
+    });
+});
+
+describe('async repeat', () => {
+});
