0c92ec29631e91ef51469a9b2fa8fb152bd0e392
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-17 06:37:39 +0000

    timing implemented

--- src/ops/timing.ts
+++ src/ops/timing.ts
@@ -1,17 +1,45 @@
 import {IterationState, Operation} from '../types';
 import {createOperation} from '../utils';
 
+/**
+ * Value timing details, produced by the [[timing]] operator.
+ */
 export interface IValueTiming<T> {
-    value: T;
-    index: number;
-    state: IterationState;
+
+    /**
+     * Time in ms it took to get the value.
+     */
     duration: number;
+
+    /**
+     * Index of the value that was timed.
+     */
+    index: number;
+
+    /**
+     * Value that was timed.
+     */
+    value: T;
+
+    /**
+     * Iteration session state.
+     */
+    state: IterationState;
+
+    /**
+     * Iteration concurrency flag:
+     *  - `true` - synchronous iteration
+     *  - `false` - asynchronous iteration
+     */
     sync: boolean;
 }
 
 /**
  * Measures timings for each value, and provides a notification.
  *
+ * The operator doesn't affect the iteration, unless the callback function throws an error.
+ *
+ * @see [[IValueTiming]], [[onEnd]]
  * @category Sync+Async
  */
 export function timing<T>(cb: (t: IValueTiming<T>) => void): Operation<T, T> {
@@ -22,11 +50,17 @@ function timingSync<T>(iterable: Iterable<T>, cb: (t: IValueTiming<T>) => void):
     return {
         [Symbol.iterator](): Iterator<T> {
             const i = iterable[Symbol.iterator]();
-            // const state: IterationState = {};
-            // let index = 0, finished = false;
+            const state: IterationState = {};
+            let index = 0;
             return {
                 next(): IteratorResult<T> {
-                    return i.next();
+                    const start = Date.now();
+                    const a = i.next();
+                    if (!a.done) {
+                        const duration = Date.now() - start;
+                        cb({duration, index: index++, value: a.value, state, sync: true});
+                    }
+                    return a;
                 }
             };
         }
@@ -37,11 +71,18 @@ function timingAsync<T>(iterable: AsyncIterable<T>, cb: (t: IValueTiming<T>) => 
     return {
         [Symbol.asyncIterator](): AsyncIterator<T> {
             const i = iterable[Symbol.asyncIterator]();
-            // const state: IterationState = {};
-            // let index = 0, finished = false;
+            const state: IterationState = {};
+            let index = 0;
             return {
                 next(): Promise<IteratorResult<T>> {
-                    return i.next();
+                    const start = Date.now();
+                    return i.next().then(a => {
+                        if (!a.done) {
+                            const duration = Date.now() - start;
+                            cb({duration, index: index++, value: a.value, state, sync: false});
+                        }
+                        return a;
+                    });
                 }
             };
         }
