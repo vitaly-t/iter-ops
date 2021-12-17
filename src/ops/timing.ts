5c46da987734bb2aea0daf7af266e902bc228f01
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-12-17 07:05:35 +0000

    docs

--- src/ops/timing.ts
+++ src/ops/timing.ts
@@ -37,7 +37,30 @@ export interface IValueTiming<T> {
 /**
  * Measures timings for each value, and provides a notification.
  *
+ * It is mainly to help evaluate performance of asynchronous lengthy iterables,
+ * though it works synchronously also.
+ *
  * The operator doesn't affect the iteration, unless the callback function throws an error.
+ *
+ * ```ts
+ * import {pipe, map, wait, timing, catchError} from 'iter-ops';
+ *
+ * const i = pipe(
+ *     asyncIterable,
+ *     map(a => myService.requestData(a)), // map into promises
+ *     wait(), // resolve each promise
+ *     timing(t => {
+ *         if(t.duration > 3000) {
+ *             // took over 3s to get the value, needs investigation;
+ *             throw new Error(`Took too long to get value ${t.value} for index ${t.index}`);
+ *         }
+ *     }),
+ *     catchError((err, ctx) => {
+ *         console.log(err?.message || err);
+ *         throw err;
+ *     })
+ * );
+ * ```
  *
  * @see [[IValueTiming]], [[onEnd]]
  * @category Sync+Async
