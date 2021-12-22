b65240802bf53029668f80c50a22ed18d8828443
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-12-23 03:19:42 +1300

    build: add web script and web module builds

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,6 +5,9 @@
 import rollupPluginNodeResolve from "@rollup/plugin-node-resolve";
 import rollupPluginTypescript from "@rollup/plugin-typescript";
 import rollupPluginAutoExternal from "rollup-plugin-auto-external";
+// @ts-ignore
+import { terser as rollupPluginTerser } from "rollup-plugin-terser";
+import rollupPluginGzip from "rollup-plugin-gzip";
 
 import pkg from "./package.json";
 
@@ -28,15 +31,23 @@ const common = {
 /**
  * Get new instances of all the common plugins.
  */
-function getPlugins() {
+function getPlugins(tsconfig = "tsconfig.build.json") {
   return [
     rollupPluginAutoExternal(),
     rollupPluginNodeResolve(),
     rollupPluginTypescript({
-      tsconfig: "tsconfig.build.json",
+      tsconfig,
     }),
   ];
 }
+
+const copyright = `/*!
+ * ${pkg.name} v${pkg.version}
+ * Copyright ${new Date().getFullYear()} ${pkg.author.name}
+ * Released under the ${pkg.license} License
+ * ${pkg.homepage}
+ */
+`;
 
 /**
  * The common JS build.
@@ -68,4 +79,53 @@ const esm = {
   plugins: getPlugins(),
 };
 
-export default [cjs, esm];
+/**
+ * The web script build.
+ */
+const web = {
+  ...common,
+
+  output: {
+    ...common.output,
+    sourcemap: true,
+    file: "dist/web/iter-ops.min.js",
+    format: "iife",
+    name: "iterOps",
+    banner: copyright,
+  },
+
+  plugins: [
+    ...getPlugins("tsconfig.build.web.json"),
+    rollupPluginTerser({
+      output: {
+        comments: "some",
+      },
+    }),
+    rollupPluginGzip(),
+  ],
+};
+
+/**
+ * The web module build.
+ */
+const webModule = {
+  ...web,
+
+  output: {
+    ...web.output,
+    file: "dist/web/iter-ops.min.mjs",
+    format: "esm",
+  },
+
+  plugins: [
+    ...getPlugins("tsconfig.build.web.json"),
+    rollupPluginTerser({
+      output: {
+        comments: "some",
+      },
+    }),
+    rollupPluginGzip(),
+  ],
+};
+
+export default [cjs, esm, web, webModule];
