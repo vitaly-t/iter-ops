8260da8f2434bed9a6233dd56258b8cd9153b698
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-10-10 04:56:29 +0100

    adding tests for flatMap #108

--- test/ops/flat-map/async.ts
+++ test/ops/flat-map/async.ts
@@ -1,4 +1,40 @@
 import {_async, _asyncValues, expect} from '../../header';
 import {flatMap, pipeAsync} from '../../../src';
 
-export default () => {};
+export default () => {
+    it('must flatten all iterables', async () => {
+        const output = pipeAsync(
+            _async([1, 2, 'one', _async('two'), 'three']),
+            flatMap((a) => a)
+        );
+        expect(await _asyncValues(output)).to.eql([
+            1,
+            2,
+            'o',
+            'n',
+            'e',
+            't',
+            'w',
+            'o',
+            't',
+            'h',
+            'r',
+            'e',
+            'e',
+        ]);
+    });
+    it('must spread sync-mapped values', async () => {
+        const output = pipeAsync(
+            ['one', 1, 2, [3]],
+            flatMap((a) => [a])
+        );
+        expect(await _asyncValues(output)).to.eql(['one', 1, 2, [3]]);
+    });
+    it('must spread async-mapped values', async () => {
+        const output = pipeAsync(
+            ['one', 1, 2, [3]],
+            flatMap(async (a) => [a])
+        );
+        expect(await _asyncValues(output)).to.eql(['one', 1, 2, [3]]);
+    });
+};
