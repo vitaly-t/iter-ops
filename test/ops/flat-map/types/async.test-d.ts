3bccee7dc6717a16e11e7c5578d6195d97396e94
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-10-11 05:18:10 +0100

    fixing doc issue

--- test/ops/flat-map/types/async.test-d.ts
+++ test/ops/flat-map/types/async.test-d.ts
@@ -4,7 +4,9 @@ import {AsyncIterableExt, flatMap, pipeAsync} from '../../../../src';
 
 declare const iterable1: AsyncIterable<AsyncIterable<number>>;
 declare const iterable2: AsyncIterable<Iterable<number>>;
-declare const iterable3: AsyncIterable<AsyncIterable<number> | Iterable<string>>;
+declare const iterable3: AsyncIterable<
+    AsyncIterable<number> | Iterable<string>
+>;
 
 const test1 = pipeAsync(
     iterable1,
