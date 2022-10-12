defc8dd758a75fce1c907d74ca045da8f3abccab
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-10-12 21:34:24 +1300

    fix: typings of flatmap operator

--- test/ops/flat-map/types/async.test-d.ts
+++ test/ops/flat-map/types/async.test-d.ts
@@ -1,31 +1,36 @@
-import {expectType} from 'tsd';
+import {expectAssignable, expectType} from 'tsd';
 
 import {AsyncIterableExt, flatMap, pipeAsync} from '../../../../src';
 
 declare const iterable1: AsyncIterable<AsyncIterable<number>>;
 declare const iterable2: AsyncIterable<Iterable<number>>;
 declare const iterable3: AsyncIterable<
-    AsyncIterable<number> | Iterable<string>
+    AsyncIterable<number> | Iterable<string> | boolean
 >;
 
 const test1 = pipeAsync(
     iterable1,
-    flatMap(() => 123)
+    flatMap((value) => {
+        expectType<number>(value);
+        return 123;
+    })
 );
 expectType<AsyncIterableExt<number>>(test1);
 
 const test2 = pipeAsync(
     iterable2,
-    flatMap(() => 123)
+    flatMap((value) => {
+        expectType<number>(value);
+        return 123;
+    })
 );
 expectType<AsyncIterableExt<number>>(test2);
 
 const test3 = pipeAsync(
     iterable3,
     flatMap((value, index) => {
+        expectType<number | string | boolean>(value);
         return index ? 123 : 'bla';
     })
 );
-
-// TODO: Why is this failing?
-// expectType<AsyncIterableExt<number | string>>(test3);
+expectAssignable<AsyncIterableExt<number | string>>(test3);
