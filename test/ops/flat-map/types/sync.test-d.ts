f2e098c359ca9877429524a187e8c098fbab6cc8
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-10-13 15:02:20 +1300

    fix: typings of flatMap operator

--- test/ops/flat-map/types/sync.test-d.ts
+++ test/ops/flat-map/types/sync.test-d.ts
@@ -3,12 +3,32 @@ import {expectType} from 'tsd';
 import {flatMap, IterableExt, pipeSync} from '../../../../src';
 
 declare const iterableNumber: Iterable<Iterable<number>>;
+declare const iterableEntries: IterableIterator<[string, number]>;
 
 const test1 = pipeSync(
     iterableNumber,
     flatMap((value) => {
-        expectType<number>(value);
+        expectType<Iterable<number>>(value);
         return 123;
     })
 );
 expectType<IterableExt<number>>(test1);
+
+const test2 = pipeSync(
+    iterableNumber,
+    flatMap((value) => {
+        expectType<Iterable<number>>(value);
+        return [1, 2, 3];
+    })
+);
+expectType<IterableExt<number>>(test2);
+
+const test3 = pipeSync(
+    iterableEntries,
+    flatMap(([key, value]) => {
+        expectType<string>(key);
+        expectType<number>(value);
+        return [1, 2, 3];
+    })
+);
+expectType<IterableExt<number>>(test3);
