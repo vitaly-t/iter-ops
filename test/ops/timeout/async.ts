ec17a4035bfd7a3274bcbd63ce366082324e8013
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-10-16 09:28:34 +0100

    tuning tests

--- test/ops/timeout/async.ts
+++ test/ops/timeout/async.ts
@@ -3,7 +3,7 @@ import {pipe, delay, timeout} from '../../../src';
 
 export default () => {
     it('must end iteration after timeout', async () => {
-        const i = pipe(_async([1, 2, 3]), delay(10), timeout(23));
+        const i = pipe(_async([1, 2, 3]), delay(10), timeout(22));
         expect(await _asyncValues(i)).to.eql([1, 2]);
     });
     it('must invoke callback on timeout', async () => {
@@ -11,7 +11,7 @@ export default () => {
         const i = pipe(
             _async([1, 2, 3]),
             delay(10),
-            timeout(23, (c) => {
+            timeout(22, (c) => {
                 count = c;
             })
         );
