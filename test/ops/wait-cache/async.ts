438c032b2ae67f8f1b5a72a9332d3a5d47465581
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-03 17:54:25 +0000

    docs + version

--- test/ops/wait-cache/async.ts
+++ test/ops/wait-cache/async.ts
@@ -11,14 +11,14 @@ export default () => {
         expect(await _asyncValues(i)).to.have.members([1, 2, 3, 4]);
     });
     it('must resolve all simple values', async () => {
-        const i = pipe(
-            _async([1, 2, 3, 4]),
-            waitCache(2)
-        );
+        const i = pipe(_async([1, 2, 3, 4]), waitCache(2));
         expect(await _asyncValues(i)).to.have.members([1, 2, 3, 4]);
     });
     it('must resolve combinations of promises and simple values', async () => {
-        const i = pipe(_async([1, Promise.resolve(2), 3, Promise.resolve(4)]), waitCache(2));
+        const i = pipe(
+            _async([1, Promise.resolve(2), 3, Promise.resolve(4)]),
+            waitCache(2)
+        );
         expect(await _asyncValues(i)).to.have.members([1, 2, 3, 4]);
     });
 };
