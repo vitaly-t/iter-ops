c50f92408aa9328c819bba3d4e1b169c582ff80d
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-04 20:10:45 +0000

    docs

--- src/ops/map.ts
+++ src/ops/map.ts
@@ -7,13 +7,15 @@ import {createOperation} from '../utils';
  *
  * When the callback is asynchronous (returns a `Promise`), the operator does not resolve the value,
  * because this library gives you the flexibility of resolving those in a variety of ways:
- *  - you can follow it up with operator {@link wait} when you want a simple one-by-one resolution;
- *  - you can follow it up with operator {@link waitRace} when you want values emitted as they resolve;
- *  - you can follow it up with {@link aggregate} + `Promise.all`, if you want to group the resolution;
- *  - you can provide any custom resolution logic.
+ *  - you can follow it up with operator {@link wait} when you want a simple one-by-one resolution
+ *  - you can follow it up with operator {@link waitRace} when you want values emitted as they resolve
+ *  - you can follow it up with {@link aggregate} + `Promise.all`, if you want to group the resolution
+ *  - you can provide any custom resolution logic
  *
  * @see
  *  - {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map Array.map}
+ *  - {@link wait}
+ *  - {@link waitRace}
  * @category Sync+Async
  */
 export function map<T, R>(
