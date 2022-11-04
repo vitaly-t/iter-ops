d17b0c4e80bd5339c0cbc6f41c0839a3b20acf54
Vitaly Tomilov <vitalytomilov@Graces-iPhone6.localdomain>
2022-11-04 19:06:56 +0000

    docs

--- src/ops/map.ts
+++ src/ops/map.ts
@@ -5,6 +5,13 @@ import {createOperation} from '../utils';
  * Standard {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map Array.map} logic for the iterable,
  * extended for supporting iteration state.
  *
+ * When the callback is asynchronous (returns a `Promise`), the operator does not resolve the value,
+ * because this library gives you the flexibility of resolving those in a variety of ways:
+ *  - you can follow it up with operator {@link wait} when you want a simple one-by-one resolution;
+ *  - you can follow it up with operator {@link waitRace} when you want values emitted as they resolve;
+ *  - you can follow it up with {@link aggregate} + `Promise.all`, if you want to group the resolution;
+ *  - you can provide any custom resolution logic.
+ *
  * @see
  *  - {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map Array.map}
  * @category Sync+Async
