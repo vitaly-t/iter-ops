cf3e4a3577ecfaa10fda357ff240c32cf6877009
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-06 17:38:42 +0000

    adding support for EXCLUDE_RACE_TESTS

--- test/ops/timeout/index.spec.ts
+++ test/ops/timeout/index.spec.ts
@@ -3,5 +3,8 @@ import sync from './sync';
 
 describe('timeout', () => {
     describe('sync', sync);
-    describe('async', async);
+    if (!process.env.EXCLUDE_RACE_TESTS) {
+        // timeout racing tests get constantly screwed in test environment
+        describe('async', async);
+    }
 });
