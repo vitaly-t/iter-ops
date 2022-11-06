50218082f5bc33653d86d421db023ca5dc522ee9
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-11-06 15:58:15 +0000

    adding tests

--- test/ops/timeout/async.ts
+++ test/ops/timeout/async.ts
@@ -1,27 +1,27 @@
-import {_async, _asyncValues, expect} from '../../header';
-import {pipe, delay, timeout} from '../../../src';
+import {_asyncValues, expect} from '../../header';
+import {pipeAsync, delay, timeout} from '../../../src';
 
 export default () => {
     it('must end iteration after timeout', async () => {
-        const i = pipe(_async([1, 2, 3]), delay(10), timeout(19));
+        const i = pipeAsync([1, 2, 3], delay(20), timeout(50));
         expect(await _asyncValues(i)).to.eql([1, 2]);
     });
     it('must invoke callback on timeout', async () => {
         let count;
-        const i = pipe(
-            _async([1, 2, 3]),
+        const i = pipeAsync(
+            [1, 2, 3],
             delay(10),
             timeout(19, (c) => {
                 count = c;
             })
         );
-        await _asyncValues(i); // iterate
+        await _asyncValues(i);
         expect(count).to.eql(2);
     });
     it('must not invoke callback without timeout', async () => {
         let invoked = false;
-        const i = pipe(
-            _async([1, 2, 3]),
+        const i = pipeAsync(
+            [1, 2, 3],
             timeout(10, () => {
                 invoked = true;
             })
@@ -30,7 +30,7 @@ export default () => {
         expect(invoked).to.be.false; // no callback
     });
     it('must deactivate on negative timeouts', async () => {
-        const i = pipe(_async([1, 2, 3]), timeout(-1));
+        const i = pipeAsync([1, 2, 3], timeout(-1));
         expect(await _asyncValues(i)).to.eql([1, 2, 3]);
     });
 };
--- test/ops/wait-race/async.ts
+++ test/ops/wait-race/async.ts
@@ -1,32 +1,32 @@
-import {_async, _asyncValues, expect} from '../../header';
-import {pipe, map, waitRace} from '../../../src';
+import {_asyncValues, expect} from '../../header';
+import {pipeAsync, map, waitRace} from '../../../src';
 
 export default () => {
     it('must resolve all promises', async () => {
-        const i = pipe(
-            _async([1, 2, 3, 4]),
+        const i = pipeAsync(
+            [1, 2, 3, 4],
             map((a) => Promise.resolve(a)),
             waitRace(2)
         );
         expect(await _asyncValues(i)).to.have.members([1, 2, 3, 4]);
     });
     it('must resolve all simple values', async () => {
-        const i = pipe(_async([1, 2, 3, 4]), waitRace(2));
+        const i = pipeAsync([1, 2, 3, 4], waitRace(2));
         expect(await _asyncValues(i)).to.have.members([1, 2, 3, 4]);
     });
     it('must resolve combinations of promises and simple values', async () => {
-        const i = pipe(
-            _async([1, Promise.resolve(2), 3, Promise.resolve(4)]),
+        const i = pipeAsync(
+            [1, Promise.resolve(2), 3, Promise.resolve(4)],
             waitRace(2)
         );
         expect(await _asyncValues(i)).to.have.members([1, 2, 3, 4]);
     });
     it('must handle invalid size of cache', async () => {
-        const i = pipe(_async([1, 2, 3, 4]), waitRace(-1));
+        const i = pipeAsync([1, 2, 3, 4], waitRace(-1));
         expect(await _asyncValues(i)).to.have.members([1, 2, 3, 4]);
     });
     it('must reject when a value rejects', async () => {
-        const i = pipe(_async([1, Promise.reject(2) as any, 3]), waitRace(1));
+        const i = pipeAsync([1, Promise.reject(2) as any, 3], waitRace(1));
         let err;
         try {
             await _asyncValues(i);
@@ -34,5 +34,9 @@ export default () => {
             err = e;
         }
         expect(err).to.eql(2);
+    });
+    it('must start resolving without delay', () => {
+        const input = [1, 2, 3, 4, 5];
+        // const i =
     });
 };
