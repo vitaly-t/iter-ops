edfea1455897e56ec50dab6e770f5f3b5697f577
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2022-12-26 13:01:59 +0000

    first take at #221

--- test/ops/timeout/async.ts
+++ test/ops/timeout/async.ts
@@ -3,26 +3,30 @@ import {pipeAsync, delay, timeout} from '../../../src';
 
 export default () => {
     it('must end iteration after timeout', async () => {
-        const i = pipeAsync([1, 2, 3], delay(20), timeout(39));
+        const i = pipeAsync([1, 2, 3], delay(10), timeout(29));
         expect(await _asyncValues(i)).to.eql([1, 2]);
+    });
+    it('must emit nothing when timeout does not permit', async () => {
+        const i = pipeAsync([1, 2, 3], delay(10), timeout(1));
+        expect(await _asyncValues(i)).to.eql([]);
     });
     it('must invoke callback on timeout', async () => {
         let count;
         const i = pipeAsync(
             [1, 2, 3],
             delay(10),
-            timeout(19, (c) => {
+            timeout(29, (c) => {
                 count = c;
             })
         );
-        await _asyncValues(i);
+        expect(await _asyncValues(i)).to.eql([1, 2]);
         expect(count).to.eql(2);
     });
     it('must not invoke callback without timeout', async () => {
         let invoked = false;
         const i = pipeAsync(
             [1, 2, 3],
-            timeout(10, () => {
+            timeout(1, () => {
                 invoked = true;
             })
         );
